0ec4c5be54e517591ebdf399e7b0178eedcde5d3
StellahIreri <stellahmireri@gmail.com>
2023-05-18 16:59:37 +0300

    uno

--- src/components/About.js
+++ src/components/About.js
@@ -5,7 +5,8 @@ function About() {
     <div id="about">
       <h2>About Me</h2>
       <p>I Made This</p>
-      <img src={image} alt="I made this" />
+      <img src="https://i.pinimg.com/564x/9b/9d/c3/9b9dc38a331603569480f2c53f4c9791.jpg" alt="I made this" />
     </div>
   );
 }
+export default About;
